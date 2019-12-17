112a6bed9f83cc9753177d2adf472fa5660c374d
gianni <gianlucascolaro@sirius.com.ar>
2019-12-17 12:39:41 +0100

    Modified to use Visual Recog service

---
+++ src/clasify-image.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+const VisualRecognitionV3 = require('ibm-watson/visual-recognition/v3');
+const { IamAuthenticator } = require('ibm-watson/auth');
+// const ai = require('./config/ai-params.json');
+
+/*const visualRecognition = new VisualRecognitionV3({
+    version: ai.version,
+    authenticator: new IamAuthenticator({
+        apikey: ai.apikey,
+    }),
+    url: ai.url,
+});*/
+
+const visualRecognition = new VisualRecognitionV3({
+    version: '2018-03-19',
+    authenticator: new IamAuthenticator({
+        apikey: '',
+    }),
+    url: '',
+});
+
+/**
+ * Helper
+ * @param {*} errorMessage
+ * @param {*} url
+ */
+function getTheErrorResponse(errorMessage, url) {
+    return {
+        statusCode: 500,
+        body: {
+            url: url || '',
+            errorMessage: errorMessage
+        }
+    };
+}
+
+/**
+ *
+ * main() will be run when the action is invoked
+ *
+ * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
+ *
+ * @return The output of this action, which must be a JSON object.
+ *
+ */
+function main(params) {
+
+    return new Promise(function (resolve, reject) {
+
+        try {
+
+            const classifyParams = {
+                url: params.url,
+                classifier_ids: ['person'],
+            };
+
+            visualRecognition.classify(classifyParams)
+                .then(classifiedImages => {
+                    console.log(JSON.stringify(classifiedImages.result, null, 2));
+                    const res = classifiedImages.result.images[0].classifiers[0].classes;
+                    const validRes = res.filter( cl => cl.class === 'person' && cl.score >= 0.5);
+                    resolve({
+                        statusCode: 200,
+                        body: {
+                            array: validRes
+                            // class: res.class,
+                            // score: res.score,
+                        },
+                        headers: { 'Content-Type': 'application/json' }
+                    });
+                })
+                .catch(err => {
+                    console.error('Error while initializing the AI service', err);
+                    resolve(getTheErrorResponse('Error while classifying image', params));
+                });
+
+        } catch (err) {
+            console.error('Error while initializing the AI service', err);
+            resolve(getTheErrorResponse('Error while communicating with the visual recognition service', classifyParams.url));
+        }
+    });
+}
---
+++ update.sh
@@ -0,0 +1,1 @@
+ibmcloud fn action update visual-recog-actions/clasify-image --docker giansco/cloud-functions-ai-visual-recognition:latest src/clasify-image.js --web true
